d1002aff003ff333521d6e52a64398d560691b9d
ianrelecker <ianrelecker@gmail.com>
2020-10-13 11:28:54 -0700

    V1

--- src/HashMap/HashMap.java
+++ src/HashMap/HashMap.java
@@ -2,29 +2,29 @@ package HashMap;
 
 import DoubleLinkedList.DoubleLinkedList;
 
-public class HashMap<KEYTYPE, DATATYPE> extends HMBase {
-    int size = N;
-
+public class HashMap<KEYTYPE, DATATYPE> extends HMBase<KEYTYPE, DATATYPE> {
 
     public HashMap(int size) {
         N = size;
+        hashmap = new DoubleLinkedList[N];
         for (int i =0; i<size; i++){
-            hashmap[i] = new DoubleLinkedList<Node>();
+            hashmap[i] = new DoubleLinkedList<Node<KEYTYPE, DATATYPE>>();
         }
     }
 
     @Override
     protected int hash(Object key) {
         //_+ first hash function
-//        /**
+        /**
         String s = key.toString();
         int sum=0;
         for (int i=0; i<s.length(); i++){
             sum += s.charAt(i);
-        }
-//         **/
+         }
+         return sum % N;
+         **/
         //_+ second hash function
-        /**
+//        /**
         String s = key.toString();
         int h = 0, high;
         for(int i= 0; i< s.length(); i++) {
@@ -35,21 +35,21 @@ public class HashMap<KEYTYPE, DATATYPE> extends HMBase {
             h&= ~high;
         }
 
-        **/
-        return sum % N;
+//        **/
+        return h % N;
     }
 
     @Override
-    public void add(Object key, Object e) {
-//        Node insert = new Node<KEYTYPE, DATATYPE>(key, e);
-//        hashmap[hash(key)] = ;
+    public void add(KEYTYPE key, DATATYPE e) {
+        Node insert = new Node(key, e);
+        hashmap[hash(key)].add(insert);
     }
 
 
     @Override
     public int[] getSizes() {
-        int[] returnSize = new int[size];
-        for (int i=0; i<size; i++){
+        int[] returnSize = new int[N];
+        for (int i=0; i<N; i++){
             returnSize[i] = hashmap[i].size();
         }
         return returnSize;
